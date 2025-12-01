ed929116c20743b0e0bbe5a805edd205f4c90bbc
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2025-12-01 16:28:25 -0300

    Parse file and store result
    parse_fille needs implementation

--- budget_tracker/cli.py
+++ budget_tracker/cli.py
@@ -1,13 +1,19 @@
+# budget_tracker/cli.py
 import typer
+from budget_tracker.parsers.parser import parse_file
 
 app = typer.Typer(help="Personal Budget Tracker")
+
 
 @app.command()
 def parse(file_path: str):
     """Parse a bank statement file"""
     typer.echo(f"Received file path: {file_path}")
-    typer.echo("Parse command works! Next step: real parsing logic.")
 
+    result = parse_file(file_path)
+
+    typer.echo("Parsing complete!")
+    typer.echo(f"Found {len(result['transactions'])} transactions")  # simple feedback
 
 def main():
     app()
