cb03bb1738cceb4afdafc190d4207059991e3f99
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2025-12-02 21:23:46 -0300

    Uncomment main.py content and run it's commands
    With the updated toml now the command budget-tracker points to the
    app main entry point

--- budget_tracker/main.py
+++ budget_tracker/main.py
@@ -1,24 +1,21 @@
 import typer
 
-def main():
-    print("Hello from budget-tracker!")
+app = typer.Typer()
 
-# app = typer.Typer()
+@app.callback()
+def callback():
+    """
+    Budget tracker
+    """
 
-# @app.callback()
-# def callback():
-#     """
-#     Budget tracker
-#     """
-
-# @app.command()
-# def main(name: str):
-#     print(f"Hello {name}")
+@app.command()
+def main(name: str):
+    print(f"Hello {name}")
     
-# @app.command()
-# def novo(name: str):
-#     print(f"Hello {name}")
+@app.command()
+def novo(name: str):
+    print(f"Hello {name}")
 
 
-# if __name__ == "__main__":
-#     app()
+if __name__ == "__main__":
+    app()
